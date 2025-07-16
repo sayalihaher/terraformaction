a6f2df560b0d2a64c81f439184622fac43745a28
sayalihaher <sayali.haher@gmail.com>
2025-07-16 11:36:19 +0530

    test

---
+++ .github/workflows/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {
+  storage_account_name = "sayalitestsa"
+   container_name = "sayalicont"
+   resource_group_name = "sayalirg"
+   key = "githubactions/terraform.tfstate"   
+ }
+}
+
+provider "azurerm" {
+   features {
+    
+   }
+ }
+
+
+resource "azurerm_resource_group" "rs1" {
+   name = "githubactionsrsghere1"
+   location = "East US"
+ }
